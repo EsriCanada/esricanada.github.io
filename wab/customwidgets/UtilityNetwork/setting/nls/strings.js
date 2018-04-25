b038cfd254c510f132da41917f6db2410d2bcb0c
Matt English <menglish@esri.ca>
2018-04-25 10:05:53 -0400

    Update strings.js

--- wab/customwidgets/UtilityNetwork/setting/nls/strings.js
+++ wab/customwidgets/UtilityNetwork/setting/nls/strings.js
@@ -2,7 +2,7 @@ define({
 	root: ({
 		configText: "Set config text:",
 		unFeatureServiceText: "Set Utility Network url:",
-		unTracingText: "Set tracing url:"
+		unTracingText: "Set tracing url:",
 		unAttributeTableWidgetIdText: "Set Attribute Table widget id:"
   })
 });
